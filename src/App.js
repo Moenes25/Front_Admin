3939cc09aae27c1cc8fa2b73a6f2bd7e8c58fad3
ali <alii25@barac.io>
2019-07-26 18:02:18 +0200

    add login

---
+++ src/App.js
@@ -0,0 +1,46 @@
+
+import React, { Fragment } from 'react';
+
+import Login from './Component/Login/Login';
+
+
+
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Fragment>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route
+              path="/app"
+              render={() => (
+                <Fragment>
+                 
+                  <div>
+                   
+                          return (
+                            <Switch>
+                           
+                             
+                          
+                            </Switch>
+                          );
+                        }}
+                 
+                  </div>
+                </Fragment>
+              )}
+            />
+          </Switch>
+        </Fragment>
+      </Router>
+    </div>
+  );
+}
+
+
+export default App;
--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,39 +1,84 @@
+
 import React from 'react';
-import withStyleLogin from './WithStyleLogin';
+
+
+
+
+
+import { Grid, Form, Button } from 'semantic-ui-react';
+
+
+import WithStyleLogin from './WithStyleLogin';
+
 
 class Login extends React.Component {
- 
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      errorInput: '',
+    };
+  }
 
- render() {
-  
-   return (
-     <div className={className}>
-       <Grid centered columns={1} className="Login">
-         <Grid.Row centered columns={4}>
-           <Grid.Column centered textAlign="center" width="50">
-             <NavLink to="/register">
-               <img src="/images/oyez.jpg" alt="oyez" width="70" height="70" />
-             </NavLink>
-             <Form>
-               <Form.Input icon="user" id="email" iconPosition="right" placeholder="E-mail" onChange={this.handleChange} value={email} />
-               <Form.Input icon="lock" id="password" iconPosition="right" placeholder="Mot de passe" type="password" onChange={this.handleChange} value={password} />
-               <p>{errorInput}</p>
-               <Button secondary fluid onClick={this.onSubmit}>Login</Button>
-             </Form>
-           </Grid.Column>
-         </Grid.Row>
-       </Grid>
-     </div>
-   );
- }
+
+  onSubmit = async () => {
+    const { email, password } = this.state;
+    const { history, login, updateUsersStates } = this.props;
+    const result = await login(email, password);
+   
+
+    if (!result.data.login.errors) {
+     const typeUser = result.data.login.user.type;
+      await updateUsersStates(typeUser);
+      if (typeUser === 'admin') {
+        history.push('/app/Home');
+      } else if (typeUser === 'rh') {
+        history.push('/app/planetes-list');
+      // eslint-disable-next-line no-empty
+      } else if (typeUser === 'Recuiteur') {
+
+      } else {
+        history.push('/');
+      }
+    } else {
+      this.setState({ errorInput: 'login ou mot de passe incorrect' });
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
+  }
+
+  render() {
+    // eslint-disable-next-line react/prop-types
+    const { className } = this.props;
+    const { email, password, errorInput } = this.state;
+    return (
+      <div className={className}>
+        <Grid centered columns={1} className="Login">
+          <Grid.Row centered columns={4}>
+            <Grid.Column centered textAlign="center" width="50">
+             
+                <img src="/images/barac.jpg" alt="barac" width="70" height="70" />
+            
+              <Form>
+                <Form.Input icon="user" id="email" iconPosition="right" placeholder="E-mail" onChange={this.handleChange} value={email} />
+                <Form.Input icon="lock" id="password" iconPosition="right" placeholder="Mot de passe" type="password" onChange={this.handleChange} value={password} />
+               
+                <p>{errorInput}</p>
+                <Button secondary fluid onClick={this.onSubmit}>Login</Button>
+              </Form>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    );
+  }
 }
 
-export default ( 
- 
 
 
-
-
-withStyleLogin, withRouter)(Login);
-
+export default WithStyleLogin (Login);
